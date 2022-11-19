635f9c1b88683f90838b0cb15923660254a3780e
jaimesan1231 <jaimesan1231@gmail.com>
2022-11-19 02:22:13 -0500

    feat: agregar submit form

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,11 @@ let closeIcon = document.querySelector(".popup__close-icon");
 let popup = document.querySelector(".popup");
 let nameInput = document.querySelector("#name");
 let descriptionInput = document.querySelector("#description");
+let formElement = document.querySelector(".form");
+let name = document.querySelector(".profile__name");
+let description = document.querySelector(".profile__description");
 
 function showPopup() {
-  let name = document.querySelector(".profile__name");
-  let description = document.querySelector(".profile__description");
   nameInput.value = name.textContent;
   descriptionInput.value = description.textContent;
   popup.classList.add("popup_opened");
@@ -16,5 +17,13 @@ function hidePopup() {
   popup.classList.remove("popup_opened");
 }
 
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
+  name.textContent = nameInput.value;
+  description.textContent = descriptionInput.value;
+  hidePopup();
+}
+
 editButton.addEventListener("click", showPopup);
 closeIcon.addEventListener("click", hidePopup);
+formElement.addEventListener("submit", handleProfileFormSubmit);
