72a920beb9e606e5acdb682da5c3273e8ec51d68
jaimesan1231 <jaimesan1231@gmail.com>
2022-12-02 18:26:54 -0500

    feat: agregar formulario de añadir

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
-let editButton = document.querySelector(".profile__edit-button");
-let closeIcon = document.querySelector(".popup__close-icon");
-let popup = document.querySelector(".popup");
-let nameInput = document.querySelector("#name");
-let descriptionInput = document.querySelector("#description");
-let formElement = document.querySelector(".form");
-let name = document.querySelector(".profile__name");
-let description = document.querySelector(".profile__description");
+const editButton = document.querySelector(".profile__edit-button");
+const addButton = document.querySelector(".profile__add-button");
+const closeIcon = document.querySelectorAll(".popup__close-icon");
+const editPopup = document.querySelector("#edit-popup");
+const addPopup = document.querySelector("#add-popup");
+const nameInput = document.querySelector("#name");
+const descriptionInput = document.querySelector("#description");
+const addForm = document.querySelector("#add-form");
+const editForm = document.querySelector("#edit-form");
+const name = document.querySelector(".profile__name");
+const description = document.querySelector(".profile__description");
 const cardTemplate = document.querySelector("#element-template").content;
 const cardsContainer = document.querySelector(".elements");
 
@@ -36,34 +39,51 @@ const initialCards = [
   },
 ];
 
-function showPopup() {
+function showEditPopup() {
   nameInput.value = name.textContent;
   descriptionInput.value = description.textContent;
-  popup.classList.add("popup_opened");
+  editPopup.classList.add("popup_opened");
+}
+function showAddPopup() {
+  addPopup.classList.add("popup_opened");
 }
 
-function hidePopup() {
-  popup.classList.remove("popup_opened");
+function hidePopup(evt) {
+  const poupContainer = evt.target.closest(".popup");
+  poupContainer.classList.remove("popup_opened");
 }
 const addCard = (card) => {
   const listElement = cardTemplate.cloneNode(true);
   listElement.querySelector(".element__image").src = card.link;
   listElement.querySelector(".element__image").alt = "Imagen de " + card.name;
   listElement.querySelector(".element__title").textContent = card.name;
-  cardsContainer.append(listElement);
+  cardsContainer.prepend(listElement);
 };
 
-function handleProfileFormSubmit(e) {
+function handleEditFormSubmit(e) {
   e.preventDefault();
   name.textContent = nameInput.value;
   description.textContent = descriptionInput.value;
-  hidePopup();
+  hidePopup(e);
+}
+function handleAddFormSubmit(e) {
+  e.preventDefault();
+  const titleInput = document.querySelector("#title");
+  const linkInput = document.querySelector("#link");
+  addCard({ name: titleInput.value, link: linkInput.value });
+  titleInput.value = "";
+  linkInput.value = "";
+  hidePopup(e);
 }
 document.addEventListener("DOMContentLoaded", () => {
   initialCards.forEach((card) => {
     addCard(card);
   });
 });
-editButton.addEventListener("click", showPopup);
-closeIcon.addEventListener("click", hidePopup);
-formElement.addEventListener("submit", handleProfileFormSubmit);
+editButton.addEventListener("click", showEditPopup);
+editForm.addEventListener("submit", handleEditFormSubmit);
+addForm.addEventListener("submit", handleAddFormSubmit);
+Array.from(closeIcon).forEach((button) => {
+  button.addEventListener("click", hidePopup);
+});
+addButton.addEventListener("click", showAddPopup);
