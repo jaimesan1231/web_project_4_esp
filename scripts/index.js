dc2aaea4dedcaeb4f96de23233e923fce49055d5
jaimesan1231 <jaimesan1231@gmail.com>
2022-12-02 18:13:58 -0500

    feat: agregar carga de fotos iniciales

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,35 @@ let descriptionInput = document.querySelector("#description");
 let formElement = document.querySelector(".form");
 let name = document.querySelector(".profile__name");
 let description = document.querySelector(".profile__description");
+const cardTemplate = document.querySelector("#element-template").content;
+const cardsContainer = document.querySelector(".elements");
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
 function showPopup() {
   nameInput.value = name.textContent;
@@ -16,6 +45,13 @@ function showPopup() {
 function hidePopup() {
   popup.classList.remove("popup_opened");
 }
+const addCard = (card) => {
+  const listElement = cardTemplate.cloneNode(true);
+  listElement.querySelector(".element__image").src = card.link;
+  listElement.querySelector(".element__image").alt = "Imagen de " + card.name;
+  listElement.querySelector(".element__title").textContent = card.name;
+  cardsContainer.append(listElement);
+};
 
 function handleProfileFormSubmit(e) {
   e.preventDefault();
@@ -23,7 +59,11 @@ function handleProfileFormSubmit(e) {
   description.textContent = descriptionInput.value;
   hidePopup();
 }
-
+document.addEventListener("DOMContentLoaded", () => {
+  initialCards.forEach((card) => {
+    addCard(card);
+  });
+});
 editButton.addEventListener("click", showPopup);
 closeIcon.addEventListener("click", hidePopup);
 formElement.addEventListener("submit", handleProfileFormSubmit);
