f0f80b7c0f956b8315474f3ac304a3c37ab09735
jaimesan1231 <jaimesan1231@gmail.com>
2023-01-31 18:26:47 -0500

    configurar webpack

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+module.exports = {
+  // conecta los plugins a PostCSS
+  plugins: [
+    // conecta el autoprefixer
+    autoprefixer,
+    // pasa un objeto con distintas opciones al conectar cssnano:
+    cssnano({ preset: "default" }), // establece la configuración de minificación por defecto
+  ],
+};
